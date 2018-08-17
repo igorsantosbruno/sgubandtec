fe2bfd3acdd964fe880d3870c9958c333a1cf93c
Igor Bruno <ig40bruno@gmail.com>
2018-08-17 15:37:44 -0300

    Teste de deploy

---
+++ src/main/java/br/com/sgu/SguApplication.java
@@ -0,0 +1,12 @@
+package br.com.sgu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SguApplication.class, args);
+	}
+}
